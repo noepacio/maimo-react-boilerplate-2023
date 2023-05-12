89baf3ef1aada28647bbc1e3361d3a92e7b5d57d
unknown <noepacio1625@gmail.com>
2023-05-11 22:17:04 -0300

    develop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,11 @@ import styles from './Header.module.css';
 const Header = () => {
   return <header className={styles.header_container}>
     <h1>
-      <img src="/assets/logo512.png" alt="Logo" />
+      <img src="assets/batidor.png" alt="Logo" />
     </h1>
+    <h2>
+      Recetas de Noe
+    </h2>
     <nav>
       <ul>
         <li>Home</li>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styles from './Hero.module.css';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+  return <Hero className= {styles.hero_container}>
+    <div>
+      <img src='assets/cocina.jpg' alt='Cocina'></img>
+    </div>
+
+  </Hero>
+  
 }
 
 export default Hero
